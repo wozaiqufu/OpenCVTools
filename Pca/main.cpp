4888423f360f3b7549e2801ebf63de3158b2f72e
李阳 <liyang2008hh@126.com>
2017-09-20 11:28:21 +0800

    add Pca project

---
+++ Pca/main.cpp
@@ -0,0 +1,29 @@
+#include "opencv2/core.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+    VideoCapture cap(0);
+    if(!cap.isOpened())
+    {
+        cout<<"no camera device!"<<endl;
+        return -1;
+    }
+    Mat img;
+    namedWindow("MacCamera",1);
+    for(;;)
+    {
+        cap>>img;
+        imshow("MacCamera",img);
+        if(waitKey(30)>0)
+        {
+            break;
+        }
+    }
+    return 0;
+}
